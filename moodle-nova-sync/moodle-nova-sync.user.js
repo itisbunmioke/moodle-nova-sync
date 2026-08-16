0066d55aceb5d4fbf3a6808d3bf83080241aedc9
Bunmi Oke <itisbunmioke@gmail.com>
2026-08-15 20:05:38 -0500

    v4.9.3: switch @updateURL to raw.githubusercontent.com (remove jsDelivr dependency)

--- moodle-nova-sync/moodle-nova-sync.user.js
+++ moodle-nova-sync/moodle-nova-sync.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Moodle → Nova Grade Sync
 // @namespace    moodle-nova-sync
-// @version      4.9.2
+// @version      4.9.3
 // @description  Captures grades from Moodle gradebook and pastes them into Nova grade entry. Configure the hostnames below before installing.
 // @author       moodle-nova-sync
 // @match        *://*/*
@@ -9,8 +9,8 @@
 // @grant        GM_getValue
 // @grant        GM_notification
 // @grant        GM_addStyle
-// @updateURL    https://cdn.jsdelivr.net/gh/itisbunmioke/moodle-nova-sync@master/moodle-nova-sync/moodle-nova-sync.user.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/itisbunmioke/moodle-nova-sync@master/moodle-nova-sync/moodle-nova-sync.user.js
+// @updateURL    https://raw.githubusercontent.com/itisbunmioke/moodle-nova-sync/master/moodle-nova-sync/moodle-nova-sync.user.js
+// @downloadURL  https://raw.githubusercontent.com/itisbunmioke/moodle-nova-sync/master/moodle-nova-sync/moodle-nova-sync.user.js
 // ==/UserScript==
 // @ts-nocheck
 
